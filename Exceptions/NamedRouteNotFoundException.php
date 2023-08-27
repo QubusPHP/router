4450c3d52ce0f5decefd3b8e911a22ac94ea9e4e
Joshua Parker <joshua@joshuaparker.dev>
2023-08-27 16:08:57 -0600

    Bumped minimum PHP version and used Qubus coding standard.

--- Exceptions/NamedRouteNotFoundException.php
+++ Exceptions/NamedRouteNotFoundException.php
@@ -5,8 +5,8 @@
  *
  * @link       https://github.com/QubusPHP/router
  * @copyright  2020
- * @license    https://opensource.org/licenses/mit-license.php MIT License
  * @author     Joshua Parker <joshua@joshuaparker.dev>
+ * @license    https://opensource.org/licenses/mit-license.php MIT License
  */
 
 declare(strict_types=1);
