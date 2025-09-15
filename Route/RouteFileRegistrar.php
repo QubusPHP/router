3e7d88a312dbe165bef62ddab7b2305202485eb5
Joshua Parker <joshua@joshuaparker.dev>
2025-09-14 19:42:40 -0700

    Fixed route loading.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- Route/RouteFileRegistrar.php
+++ Route/RouteFileRegistrar.php
@@ -4,35 +4,17 @@ declare(strict_types=1);
 
 namespace Qubus\Routing\Route;
 
-use Qubus\Routing\Psr7Router;
-
 final class RouteFileRegistrar
 {
-    /**
-     * The router instance.
-     */
-    protected ?Psr7Router $router = null;
-
-    /**
-     * Create a new route file registrar instance.
-     *
-     * @param Psr7Router $router
-     */
-    public function __construct(Psr7Router $router)
-    {
-        $this->router = $router;
-    }
-
     /**
      * Require the given routes file.
      *
      * @param string $routes
-     * @return void
+     * @return mixed
      */
-    public function register(string $routes): void
+    public function register(string $routes): mixed
     {
-        $router = $this->router;
-
-        require $routes;
+        $result = require $routes;
+        return $result;
     }
 }
